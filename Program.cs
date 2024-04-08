97be8bd16fe3c0eba02385d60af720fb328f269e
jhu vitoria <jhuliavitoria226@gmail.com>
2024-04-08 16:02:39 -0300

    Concluído.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Imitando Dory");
+Console.ResetColor();
+Console.WriteLine();
+Console.Write("Digite uma frase...: ");
+ string frase = Console.ReadLine()!;
+ 
+ string frasesEmBaleies = frase
+    .Replace("a", "aaa")
+    .Replace("A", "AAA")
+    .Replace("e", "eee")
+    .Replace("E", "EEE")
+    .Replace("i", "iii")
+    .Replace("I", "III")
+    .Replace("o", "ooo")
+    .Replace("O", "OOO")
+    .Replace("u", "uuu")
+    .Replace("U", "UUU");
+
+Console.WriteLine($"\nEm Baleiês:\n\n{frasesEmBaleies}");
